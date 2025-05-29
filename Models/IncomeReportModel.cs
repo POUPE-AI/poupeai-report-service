f5cff3339ab1cd0c9b0cfdbe300c65b87d59fbc0
eudivan44 <eudivan44@gmail.com>
2025-05-29 15:46:16 -0300

    feat: created incomeservices and finished income endpoint

--- Models/IncomeReportModel.cs
+++ Models/IncomeReportModel.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -6,14 +7,14 @@ namespace poupeai_report_service.Models;
 internal class IncomeReportModel : BaseReportModel
 {
     [BsonElement("total_income")]
-    [BsonRepresentation(BsonType.Double)]
+    [JsonPropertyName("totalIncome")]
     public double TotalIncome { get; set; }
 
     [BsonElement("categories")]
-    [BsonRepresentation(BsonType.Array)]
+    [JsonPropertyName("categories")]
     public List<CategoryModel> Categories { get; set; } = [];
 
     [BsonElement("main_incomes")]
-    [BsonRepresentation(BsonType.Array)]
+    [JsonPropertyName("mainIncomes")]
     public List<TransactionModel> MainIncomes { get; set; } = [];
 }
